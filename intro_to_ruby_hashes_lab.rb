da8acd6236796969315e5cfee994603aa7a669fc
Jacky Feng <jackyfeng530@yahoo.com>
2019-09-04 18:46:53 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,7 +31,7 @@ end
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   newhash = {
-    key: value 
+    key => value 
   }
   newhash
 end
